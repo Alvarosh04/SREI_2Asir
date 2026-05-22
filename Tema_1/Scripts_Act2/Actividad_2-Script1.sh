3401a558e1758d776f419055cf30a9e84b6abe81
Alvarosh04 <alvarosanchez1708@gmail.com>
2026-05-22 13:30:52 +0200

    Enhance script with comments for clarity
    Add comments to explain script functionality and parameter check.

--- Tema_1/Scripts_Act2/Actividad_2-Script1.sh
+++ Tema_1/Scripts_Act2/Actividad_2-Script1.sh
@@ -1,5 +1,8 @@
 #!bin/bash
 
+#Crea un script que añada un puerto de escucha en el fichero de configuración de Apache. 
+#El puerto se recibirá como parámetro en la llamada y se comprobará que no esté ya presente en el fichero de configuración.
+
 if [ $# -ne 1 ]; then
     echo "Introduce solo un puerto"
     exit 1
